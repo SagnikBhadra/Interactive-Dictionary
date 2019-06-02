0f51cb8efa9e4fe2034a929beabdbace2e9653b7
Sagnik Bhadra <bhad1434@pacificu.edu>
2019-06-02 00:53:15 -0700

    Ask user if they meant best match

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def defin_of_word (word):
     if  word in data:
         return data[word]
     elif get_close_matches(word, data.keys(), cutoff=0.8):
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        yn =  input ("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
