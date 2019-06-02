40ac7c2b79a68bcd297f37cbc445771b536133a0
Sagnik Bhadra <bhad1434@pacificu.edu>
2019-06-02 00:44:15 -0700

    Recommend close matches

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,6 +8,8 @@ def defin_of_word (word):
     word = word.lower()
     if  word in data:
         return data[word]
+    elif get_close_matches(word, data.keys(), cutoff=0.8):
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word doesn't exist. Please double check it."
 
