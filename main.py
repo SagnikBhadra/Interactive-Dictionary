9961f5baa3df8b1a4eb72515c1750d075880dc42
Sagnik Bhadra <bhad1434@pacificu.edu>
2019-06-02 00:16:17 -0700

    Handle case for word does not exist

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import json
 data = json.load(open("data.json"))
 
 def defin_of_word (word):
-    return data[word]
+    if  word in data:
+        return data[word]
+    else:
+        return "The word doesn't exist. Please double check it."
 
 defin = defin_of_word(input("Enter word: "))
 print(defin)
